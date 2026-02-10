d22434e7c650d7562f34f2373a17f8357d27f221
sanket-245 <sanket45dm@gmail.com>
2026-02-11 01:15:36 +0530

    Added service registration file in repository project to add the dependencies.

---
+++ backend/Repository/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Repository.Repositories;
+using Repository.Repository_Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+
+            services.AddScoped<ITaskRepository, TaskRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
