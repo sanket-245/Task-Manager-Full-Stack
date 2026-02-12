1a91038f86d794df87d72c914d7d6f2986c68eea
sanket-245 <sanket45dm@gmail.com>
2026-02-12 23:41:54 +0530

    Added Service registration class for services

---
+++ backend/Services/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services.Services;
+using Services.Services_Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public static class ServiceRegistration
+    {
+        public static void Addserviceregistration( this IServiceCollection services)
+        {
+            _ = services.AddScoped<ITaskServices, TaskServices>();
+        }
+    }
+}
