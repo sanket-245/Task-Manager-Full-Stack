69c87ee130af303aed2ee4d296f42f2f35df06b6
sanket-245 <sanket45dm@gmail.com>
2026-02-14 02:22:42 +0530

    Removed sample code

--- backend/TaskManager.API/Program.cs
+++ backend/TaskManager.API/Program.cs
@@ -1,7 +1,7 @@
-using Repository;
-using Services;
 using DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
+using Repository;
+using Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,32 +30,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.MapControllers();
-app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run();
