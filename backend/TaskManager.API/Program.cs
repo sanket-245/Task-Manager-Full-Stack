c62cf78df0ab9f1fe4867167aa7b4af893237580
sanket-245 <sanket45dm@gmail.com>
2026-02-16 00:02:40 +0530

    Added JWT authentication settings and configured in program file

--- backend/TaskManager.API/Program.cs
+++ backend/TaskManager.API/Program.cs
@@ -1,7 +1,11 @@
 using DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Repository;
 using Services;
+using System.Text;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +13,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TaskManagerDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var jwtsettngs = builder.Configuration.GetSection("JWTSettings");
+var key = Encoding.UTF8.GetBytes(jwtsettngs["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(
+    options =>
+    {
+        options.TokenValidationParameters
+               = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+
+                   ValidIssuer = jwtsettngs["Issuer"],
+                   ValidAudience = jwtsettngs["Audience"],
+                   IssuerSigningKey = new SymmetricSecurityKey(key)
+               };
+    });
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -30,6 +58,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
+
+app.UseAuthentication();   // MUST come before UseAuthorization
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
